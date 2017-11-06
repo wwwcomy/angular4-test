ccaa241a7d76e1402f1a8c80102ac38c0d4e7f4b
wwwcomy <wwwcomy@gmail.com>
2017-11-06 13:17:56 +0800

    List and editable

--- src/app/HEROES.ts
+++ src/app/HEROES.ts
@@ -1,4 +1,6 @@
-export const HEROES = [
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
