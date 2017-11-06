f3d7f3048197ff6bced951b3f91b43cf0677eebb
wwwcomy <wwwcomy@gmail.com>
2017-11-06 10:01:19 +0800

    Hero list v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  	<h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+	  <li *ngFor="let hero1 of heroes">
+	  	<span class="badge">{{hero1.id}}</span> {{hero1.name}}
+	  </li>
+	</ul>
+    `
 })
+
+
+
 export class AppComponent {
   title = 'This is my first angular App';
+  heroes = HEROES;
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
